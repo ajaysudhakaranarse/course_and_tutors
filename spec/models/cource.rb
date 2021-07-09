8f04b8c012a4abb81def03636fae5fbeb6f340bf
Ajay Anarse <ajay@joshsoftware.com>
2021-07-09 21:52:46 +0530

    Added test cases for Tutor model

---
+++ spec/models/cource.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe Course, type: :model do
+  describe 'validation' do
+    it { should validate_uniqueness_of(:name) }
+  end
+end
---
+++ spec/models/tutor.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe Tutor, type: :model do
+  describe 'validation' do
+    it { should validate_uniqueness_of(:mobile) }
+
+    it { should allow_value("9812121212").for(:mobile) }
+
+    it { should_not allow_value("98121212abc").for(:mobile) }
+
+    it { should_not allow_value("6812121212").for(:mobile) }
+
+    context 'tutor_assign_to_course?' do
+      before do
+        @course = create(:course)
+        create(:tutor, course: @course)
+      end
+
+      it 'return tutor already assign to course' do
+        params = { 
+          course: 
+            { name: "test1", 
+              tutors_attributes: 
+              [
+                { name: 'test_name', mobile: '9812121213'}
+              ] 
+            }   
+          }
+        course = Course.new(params[:course])
+        course.save
+        expect(course.errors.messages[:'tutors[0].course_id']).to eq(["tutor already assigned to #{@course.name}"])
+      end
+    end
+  end
+end
