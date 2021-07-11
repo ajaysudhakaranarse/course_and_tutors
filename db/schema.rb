9044efbbc0f8417b8be5e84c49e1c1ff5cfd21c5
Ajay Anarse <ajay@joshsoftware.com>
2021-07-11 14:54:53 +0530

    Used pg gem

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20210709131920) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "courses", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -21,7 +24,7 @@ ActiveRecord::Schema.define(version: 20210709131920) do
   create_table "tutors", force: :cascade do |t|
     t.string "name"
     t.string "mobile"
-    t.integer "course_id"
+    t.bigint "course_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["course_id"], name: "index_tutors_on_course_id"
